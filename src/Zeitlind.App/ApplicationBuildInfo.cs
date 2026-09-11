aa91989f0faf1059d4b9febd643d4b821c6cf4df
GuestLiang <GuestLiang@outlook.com>
2026-09-11 22:31:47 +0100

    🔖 v2.2.3

--- src/Zeitlind.App/ApplicationBuildInfo.cs
+++ src/Zeitlind.App/ApplicationBuildInfo.cs
@@ -2,7 +2,7 @@ namespace Zeitlind.App;
 
 internal static class ApplicationBuildInfo
 {
-    public const string Version = "2.2.2";
+    public const string Version = "2.2.3";
 
 #if DEBUG
     public const string Configuration = "Debug";
